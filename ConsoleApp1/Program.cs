5847e6725932c5c1c7c90996742d2a4fd8b9613c
arsyha2003 <arsenyghoul@gmail.com>
2024-11-27 22:11:20 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        TcpClient client = new TcpClient();
+        NetworkStream stream;
+        try
+        {
+            await client.ConnectAsync(IPAddress.Loopback, 7777);
+            stream = client.GetStream();
+            await Console.Out.WriteLineAsync("Подключение успешно");
+            while (true)
+            {
+                string message = Console.ReadLine()+'\n';
+                stream.Write(Encoding.UTF8.GetBytes(message));
+            }
+        }
+        catch (Exception ex)
+        {
+            await Console.Out.WriteLineAsync(ex.Message);
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+}
